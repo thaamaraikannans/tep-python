84ce0e14e3cad01490ee1331a2b5876b5f387f15
thaamaraikannans <thaamaraikannans.xbi4@gmail.com>
2024-08-08 13:00:31 +0530

    add functions

--- basics/functions/app.py
+++ basics/functions/app.py
@@ -1,3 +1,10 @@
+# def print_something(name):
+#     print(name)
+
+# # print_something("krishna")
+# # print_something("naveen")
+# print_something("sathya")
+# print_something("kannan")
 students_grades = [
     {"name": "Alice", "marks": [{"social": 80, "maths": 60, "science": 20, "tamil": 99, "english": 66}]},
     {"name": "Bob", "marks": [{"social": 70, "maths": 65, "science": 75, "tamil": 85, "english": 78}]},
@@ -11,13 +18,11 @@ students_grades = [
     {"name": "Jack", "marks": [{"social": 78, "maths": 85, "science": 88, "tamil": 90, "english": 84}]}
 ]
 
-subjects = ["social", "maths", "science", "tamil", "english"]
-total_rank = [] # stu name and the total mark
-total_order = [] # total mark
-for student in students_grades:
-    user = []
-    name = student["name"]
-    user.append(name)
+def print_students(values):
+    for data in values:
+        print(data["name"])
+
+def total_marks(student):
     soc_m = student["marks"][0]["social"]
     mat_m = student["marks"][0]["maths"]
     sci_m = student["marks"][0]["science"]
@@ -25,32 +30,10 @@ for student in students_grades:
     eng_m = student["marks"][0]["english"]
     stu_mark = [ soc_m, mat_m, sci_m, tam_m, eng_m]
     total = sum(stu_mark)
-    user.append(total)
-    total_order.append(total)
     print(total)
-    total_rank.append(user)
-    print(user)
-    for i in range(len(stu_mark)):
-        if stu_mark[i] >= 35:
-            pass
-            # print(subjects[i] + " is " + "Pass")
-        else:
-            pass
-            # print(subjects[i] + " is " + "Fail")
-print(total_rank)
-total_order.sort() # ascesding -> lower ----- higher
-total_order.reverse() # descending -> higher ---- lower
-print(total_order)
-
-rank_holder = [] # rank wise name
-for rank in total_order:
-    for j in range(len(total_rank)):
-        if total_rank[j][1] == rank:
-            rank_holder.append(total_rank[j][0])
-
-print(rank_holder)
-
-for r_name in range(len(rank_holder)):
-    print(r_name + 1, "is ", rank_holder[r_name])
     
+print_students(students_grades)
 
+for marks in students_grades:
+    print(marks)
+    total_marks(marks)
